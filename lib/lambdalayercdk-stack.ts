a3317bf8cd12bf5057714c9a823e173eb3aa66fa
nkserveren26 <norinoritoshi@outlook.jp>
2023-06-28 22:42:05 +0900

    commit

--- lib/lambdalayercdk-stack.ts
+++ lib/lambdalayercdk-stack.ts
@@ -1,11 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
+import { AssetCode, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LambdalayercdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const lambdaParams = {
+      functionName: "lambdaFunction",
+      codePath: "lambda/test",
 
-    
+    };
+
+    const lambdafunc: Function = new Function(this, lambdaParams.functionName, {
+      functionName: lambdaParams.functionName,
+      runtime: Runtime.PYTHON_3_9,
+      code: AssetCode.fromAsset(lambdaParams.codePath),
+      handler: "index.handler",
+    });
   }
 }
